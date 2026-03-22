fb719bb40b4327e60c5383e2e531255c0e0d96de
Patrick Fischer <patrickfischer@MacBook-Pro-1546.local>
2026-03-22 14:06:56 +0100

    Slow down demo pacing

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const CARD_TURN_TIME_MS = 30_000;
 const TEXT_TURN_TIME_MS = 90_000;
 const BOT_DELAY_MIN_MS = 900;
 const BOT_DELAY_MAX_MS = 1700;
-const DEMO_DELAY_MIN_MS = 380;
-const DEMO_DELAY_MAX_MS = 760;
+const DEMO_DELAY_MIN_MS = 2200;
+const DEMO_DELAY_MAX_MS = 3400;
 
 const app = express();
 const server = http.createServer(app);
